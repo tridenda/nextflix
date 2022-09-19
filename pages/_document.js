2ce8b64c1f675399d257e374789585a5a2a2d1e6
Tri Denda <tridenda.nk@gmail.com>
2022-09-19 11:24:33 +0700

    added banner component

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
             crossOrigin="true"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&display=swap"
             rel="stylesheet"
           ></link>
         </Head>
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 import Head from "next/head";
+
+import Banner from "../components/banner/banner.component";
+
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -10,7 +13,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Nextflix</h1>
+      <h1>Netxflix</h1>
+
+      <Banner
+        title="Clifford the red dog"
+        subTitle="a very cute dog"
+        imgUrl="/static/clifford.webp"
+      />
+
+      {/*
+      <NavBar />
+      <Card />
+    */}
     </div>
   );
 }
