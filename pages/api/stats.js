5b65005b02658d7e4573c894cd27223b4f45c498
Tri Denda <tridenda.nk@gmail.com>
2022-09-28 21:42:33 +0700

    create updateStats function

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { findVideoIdByUser } from "../../lib/db/hasura";
+import { findVideoIdByUser, updateStats } from "../../lib/db/hasura";
 
 export default async function stats(req, res) {
   if (req.method === "POST") {
@@ -13,9 +13,24 @@ export default async function stats(req, res) {
       const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
       const userId = decodedToken.issuer;
       const videoId = req.query.videoId;
-      const findVideoId = await findVideoIdByUser(token, userId, videoId);
+      const doesStatsExist = await findVideoIdByUser(token, userId, videoId);
 
-      res.send({ msg: "it works", decodedToken, findVideoId });
+      if (doesStatsExist) {
+        // update it
+        const response = await updateStats(token, {
+          favoutited: 0,
+          watched: false,
+          userId,
+          videoId: "dRuwjZJ-DQw",
+        });
+
+        res.send({ msg: "Update success", updateStats: response });
+      } else {
+        // add it
+        // const response = await insertStats(token);
+
+        res.send({ msg: "it works", decodedToken, doesStatsExist });
+      }
     } catch (error) {
       console.error("Error occured /stats", error);
       res.status(500).send({ done: false, error: error?.message });
