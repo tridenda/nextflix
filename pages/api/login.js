faae98a4f8e74605993594706d610da21ee409d8
Tri Denda <tridenda.nk@gmail.com>
2022-09-28 13:55:56 +0700

    integrate Hasura and magic link to add new users

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,6 +1,8 @@
 import jwt from "jsonwebtoken";
+
 import { isNewUser, createNewUser } from "../../lib/db/hasura";
 import { magicAdmin } from "../../lib/magic";
+import { setTokenCookie } from "../../lib/cookies";
 
 export default async function login(req, res) {
   if (req.method === "POST") {
@@ -24,18 +26,13 @@ export default async function login(req, res) {
         process.env.JWT_SECRET
       );
 
-      // check if the user exists
+      // check if new user exist
       const isNewUserQuery = await isNewUser(token, metadata.issuer);
 
-      if (isNewUserQuery) {
-        // create a new user
-        const createNewUserMutation = await createNewUser(token, metadata);
-        console.log({ createNewUserMutation });
+      isNewUserQuery && (await createNewUser(token, metadata));
+      setTokenCookie(token, res);
 
-        res.send({ done: true, msg: "is a new user" });
-      } else {
-        res.send({ done: true, msg: "not a new user" });
-      }
+      res.send({ done: true });
     } catch (error) {
       console.error("Something went wrong logging in", error);
       res.status(500).send({ done: false });
